2725482a97747939c0aff36eee45985cfb36fb2d
atiugaev <atiugaev@synergy.ru>
2026-03-19 19:18:38 +0300

    documentation example

--- week_1/echo_server.ex
+++ week_1/echo_server.ex
@@ -1,4 +1,22 @@
 defmodule EchoServer do
+  @moduledoc """
+  Учебный пример "Эхо сервера"
+  который принимает :ping и отвечает {:pong, node()},
+  если не :ping, то {:error, node()}
+  ## Примеры
+  iex> pid = EchoServer.start()
+  #PID<0.114.0>
+
+  iex> send(pid, {:ping, self()}
+  {:ping, #PID<0.105.0>}
+
+  iex> receive do
+         {:pong, val} -> IO.puts "Answer #{val}"
+       end
+  Answer nonode@nohost
+  :ok
+  """
+
   def start do
     spawn(fn -> loop() end)
   end
